f0d6db05666bfc1b992d72c0f4a779d676c46fa2
hitheshamin24 <hitheshamin423@gmail.com>
2026-07-10 15:03:01 +0530

    add update funciton

--- Practice-Projects/07-ToDo-List(React)/src/App.jsx
+++ Practice-Projects/07-ToDo-List(React)/src/App.jsx
@@ -1,34 +1,69 @@
 import React, { useState } from "react";
 import Tasks from "./Tasks";
-
+import { useForm } from "react-hook-form";
 const App = () => {
-  const [taskInput, setTaskInput] = useState("");
-  const [task, setTask] = useState([]);
-  const addTask = (e) => {
-    e.preventDefault();
-    setTask([...task, taskInput]);
-    setTaskInput("");
+  const [task, setTask] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || [],
+  );
+  const [updatedData, setUpdatedData] = useState(null);
+  const {
+    register,
+    reset,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    mode: "onChange",
+   
+  });
+  const setToLocalStorage = (key, value) => {
+    localStorage.setItem(key, JSON.stringify(value));
   };
-  const deleteTask = (idx) => {
-    const updatedTask = task.filter((e, index) => index !== idx);
+  const editTask=(data)=>{
+    setUpdatedData(data)
+    reset(data)
+  }
+  const addTask = (data) => {
+    const exist = task.some((val) => val.tasks === data.tasks);
+    if (exist) return;
+    if (updatedData) {
+      let updateTask = task.map((t) =>
+        t.id === updatedData.id ? { ...data, id: updatedData.id } : t,
+      );
+      setTask(updateTask);
+      return;
+    }
+    let updatedTask = [...task, { ...data, id: Date.now() }];
+    console.log(updatedTask);
+
     setTask(updatedTask);
+    setToLocalStorage("tasks", updatedTask);
+
+    reset();
+  };
+  const deleteTask = (id) => {
+    const updatedTask = task.filter((e) => e.id !== id);
+    setTask(updatedTask);
+    setToLocalStorage("tasks", updatedTask);
   };
   return (
     <div className="min-h-screen bg-linear-to-br from-amber-50 to-orange-100">
       <div className="flex justify-center items-center h-72">
-        <form className="flex gap-3 bg-white p-4 rounded-2xl shadow-lg w-[90%] max-w-xl">
+        <form
+          onSubmit={handleSubmit(addTask)}
+          className="flex gap-3 bg-white p-4 rounded-2xl shadow-lg w-[90%] max-w-xl"
+        >
           <input
-            value={taskInput}
-            onChange={(e) => setTaskInput(e.target.value)}
+            {...register("tasks", {
+              required: "Task is required",
+            })}
             type="text"
             placeholder="Enter your task..."
             className="flex-1 px-4 py-3 border border-gray-300 rounded-xl outline-none focus:ring-2 focus:ring-amber-400"
           />
-
+          {errors.tasks && (
+            <p className="text-red-500">{errors.tasks.message}</p>
+          )}{" "}
           <button
-            onClick={(e) => {
-              addTask(e);
-            }}
             type="submit"
             className="bg-amber-500 hover:bg-amber-600 text-white font-semibold px-6 py-3 rounded-xl transition duration-300 cursor-pointer"
           >
@@ -40,7 +75,13 @@ const App = () => {
       <div className="flex flex-col items-center gap-4 pb-10">
         {task.map((elem, idx) => {
           return (
-            <Tasks key={idx} index={idx} deleteFun={deleteTask} task={elem} />
+            <Tasks
+              key={elem.id}
+              index={idx}
+              deleteFun={deleteTask}
+              task={elem}
+              editTask={editTask}
+            />
           );
         })}
       </div>
--- Practice-Projects/07-ToDo-List(React)/src/Tasks.jsx
+++ Practice-Projects/07-ToDo-List(React)/src/Tasks.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
 
-const Tasks = ({index,deleteFun,task}) => {
+const Tasks = ({deleteFun,task,editTask}) => {
   return (
     <div className="w-[90%] max-w-3xl bg-white shadow-md rounded-xl px-6 py-5 flex justify-between items-center hover:shadow-xl transition">
-              <p className="text-lg font-medium text-gray-700">{task}</p>
+              <p className="text-lg font-medium text-gray-700">{task.tasks}</p>
 
               <button
                 onClick={() => {
-                    deleteFun(index)
+                    deleteFun(task.id)
                 }}
                 className="bg-red-500 hover:bg-red-600 text-white px-5 py-2 rounded-lg transition cursor-pointer"
               >
                 Delete
+              </button>
+              <button
+                onClick={() => {
+                  editTask(task)
+              
+                }}
+                className="bg-red-500 hover:bg-red-600 text-white px-5 py-2 rounded-lg transition cursor-pointer"
+              >
+                Update
               </button>
             </div>
   )
