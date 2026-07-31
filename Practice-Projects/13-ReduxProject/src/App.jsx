0ef4672ae8a11e06a74504497a2f7882c2d9c26b
hitheshamin24 <hitheshamin423@gmail.com>
2026-07-31 15:00:33 +0530

    Api integration

--- Practice-Projects/13-ReduxProject/src/App.jsx
+++ Practice-Projects/13-ReduxProject/src/App.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { fetchGIFs, fetchPhotos, fetchVideos } from "./api/mediaApi";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const getPhotos = async () => {
+    const data=await fetchPhotos('cat');
+    console.log(data)
+  };
+const getVideos=async()=>{
+    const data=await fetchVideos("cat")
+    console.log(data)
 
-export default App
+}
+const getGIFs=async()=>{
+  const data=await fetchGIFs("cat")
+  console.log(data)
+}
+  return (
+    <div className="h-screen w-full bg-gray-700  text-white">
+      <button onClick={() => getPhotos()}>Get Photos</button>
+      <button onClick={() => getVideos()}>Get Videos</button>
+      <button onClick={() => getGIFs()}>Get Gifs</button>
+    </div>
+  );
+};
+
+export default App;
