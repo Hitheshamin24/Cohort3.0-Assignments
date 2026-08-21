9ec554e9b605dfe5b946ef92d1808845a20bebcd
hitheshamin24 <hitheshamin423@gmail.com>
2026-08-21 10:54:17 +0530

    fix decalred issues

--- Practice-Projects/Easeui-project/src/components/Input/Input.tsx
+++ Practice-Projects/Easeui-project/src/components/Input/Input.tsx
@@ -75,7 +75,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
           id={inputId}
           ref={ref}
           className={cn(inputVariants({ size, tone, disabled }), className)}
-          disabled={disabled}
+          disabled={disabled ?? undefined}
           {...props}
         />
         {error ? (
